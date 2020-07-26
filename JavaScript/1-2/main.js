e61782b4afdbd03ec6deab796277652da215f030
菅原美咲 <cassis.blueberry.nep@gmail.com>
2020-07-26 17:18:07 +0900

    課題提出
    変数・定数の課題

---
+++ JavaScript/1-2/main.js
@@ -0,0 +1,9 @@
+let name = "池田";
+console.log(name + "くん");
+let number = 10;
+console.log(number * 3);
+console.log(`${name}君は${number}月生まれです`);
+let price = 100;
+console.log(price * 2);
+price = 500;
+console.log(price * 2);
