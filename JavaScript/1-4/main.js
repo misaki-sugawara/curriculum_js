14cd827353c0865a16c14e4efae26817ff69ca3d
菅原美咲 <cassis.blueberry.nep@gmail.com>
2020-07-26 19:35:02 +0900

    1-4課題提出
    繰り返し処理の課題提出です

---
+++ JavaScript/1-4/main.js
@@ -0,0 +1,20 @@
+/*
+let number = 1;
+while(number <= 10) {
+    console.log(number);
+    number++;
+}
+*/
+
+
+for (let number = 1; number <= 100; number++){
+    if (number % 3 === 0 && number % 5 === 0){
+        console.log("FizzBuzz!");
+    }else if (number % 5 === 0){
+        console.log("Buzz!");
+    }else if (number % 3 === 0){
+        console.log("Fizz!")
+        }else{
+            console.log(number);
+        }
+    }
