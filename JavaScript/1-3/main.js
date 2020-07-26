180621316352ece94f1074c5228cf349724113bf
菅原美咲 <cassis.blueberry.nep@gmail.com>
2020-07-26 18:31:46 +0900

    1-3課題提出
    条件分岐・演算子の課題です

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,80 @@
+let num1 = 10;
+let num2 = 4;
+let result;
+//足し算
+result = num1 + num2;
+console.log(result);
+
+//引き算
+result = num1 - num2;
+console.log(result);
+
+//掛け算
+result = num1 * num2;
+console.log(result);
+
+//割り算
+result = num1 / num2;
+console.log(result);
+
+//剰余
+result = num1 % num2;
+console.log(result);
+
+let lastName = "田中 ";
+let firstName = "一郎";
+let fulName = lastName + firstName;
+console.log(fulName);
+
+let x = 10;
+x++;
+console.log(x);
+
+
+let y = 10;
+y--;
+console.log(y);
+
+
+
+let score = 90;
+if (score >= 70){
+    console.log('合格');
+}else{
+    console.log('不合格');
+}
+
+//以下課題
+
+let a = 9;
+if (a !== 10){
+    console.log(a);
+}
+
+let b =16;
+if (b >= 10 && b <= 20){
+    console.log(b);
+}
+
+let X = 10;
+if (X >= 10 && b <= 20){
+    console.log("成功です");
+}else{
+    console.log("失敗です");
+}
+
+let Y = 0
+if (Y % 2 === 0){
+    console.log("偶数です");
+}else if (Y % 2 !== 0){
+    console.log("奇数です");
+}
+
+
+
+
+
+
+
+
+
